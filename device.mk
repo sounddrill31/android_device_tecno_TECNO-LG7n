a93fbb1c9a52be34153d5721458a909f80626f59
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-18 12:52:40 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-LG7n
+LOCAL_PATH := device/tecno/LG7n
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
