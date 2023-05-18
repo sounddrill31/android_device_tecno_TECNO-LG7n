ec4cdf47b0d2b6cbeb5aa33f6a828667b82e446b
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-18 10:42:20 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-LG7n
+DEVICE_PATH := device/tecno/LG7n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
