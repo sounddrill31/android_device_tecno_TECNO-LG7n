de0ce40ae6452ed9abf715398c95320c30972b4f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-18 10:40:32 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_TECNO-LG7n.mk
+    $(LOCAL_DIR)/twrp_LG7n.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_TECNO-LG7n-user \
-    twrp_TECNO-LG7n-userdebug \
-    twrp_TECNO-LG7n-eng
+    twrp_LG7n-user \
+    twrp_LG7n-userdebug \
+    twrp_LG7n-eng
