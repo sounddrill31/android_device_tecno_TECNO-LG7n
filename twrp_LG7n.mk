70f8531c73562d5051c1a6be2e38058368271aa4
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-07-21 11:34:20 +0530

    Update twrp_LG7n.mk

--- twrp_LG7n.mk
+++ twrp_LG7n.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from TECNO-LG7n device
 $(call inherit-product, device/tecno/LG7n/device.mk)
